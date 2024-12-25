e73a4eee76f08cd0c66bacf1f0930a2dd56d5d26
Abdi-01 <abdialghi@gmail.com>
2024-12-25 18:12:31 +0700

    setup vercel update vercelignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-        "./Components/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
     },
     plugins: [],
-    important: true,
 };
