a3352ab6b1a6c8cae02cd0860c5af64db91a37e0
Abdi-01 <abdialghi@gmail.com>
2024-12-25 18:45:47 +0700

    setup vercel add security policy

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Tambahkan header Content-Security-Policy
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; style-src 'self' 'unsafe-inline' https://fixit-hub.vercel.app;");
+
+        return $response;
+    }
+}
