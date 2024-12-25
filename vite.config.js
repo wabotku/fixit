b8969850125c1026fd5233d301fa1aa50e9f4403
Abdi-01 <abdialghi@gmail.com>
2024-12-25 18:28:24 +0700

    setup vercel revamp vite.config and gitignore

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    server: {
-        host: true,
-        hmr: {
-            host: "localhost",
-        },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
